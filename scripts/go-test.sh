1a990c81736dc604984389a4312225f1d54304ee
Sam Rudge <sam.rudge@songkick.com>
2016-04-10 15:07:00 +0100

    Move gofmt to go-test script

--- scripts/go-test.sh
+++ scripts/go-test.sh
@@ -5,3 +5,9 @@ source $(dirname $0)/helpers.sh
 # Find packages excluding vendor
 KAYATRA_PACKAGES=$(go list ./... | grep -v /vendor/)
 go test -v ${KAYATRA_PACKAGES}
+
+
+GOFMT_FAILS=$(gofmt -l ${KAYATRA_PACKAGES})
+echo -e ${GOFMT_FAILS}
+
+#test $(echo -en \"${GOFMT_FAILS}\" | wc -l) -eq 0
